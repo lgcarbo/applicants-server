253440307abddba2fd54b59e671c164209598df6
Leonardo Carbonelli <leogabri61@hotmail.com>
2017-07-07 22:22:35 -0300

    Enable CORS.

--- server.babel.js
+++ server.babel.js
@@ -6,6 +6,12 @@ import { EducationLevel, SalaryType, ContractType, TechnicalSkill, Applicant, Ap
 
 const app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
